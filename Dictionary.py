0346a02c141405692c05fe61facbb05ebfd1e478
Puneet Gupta <puneetgupta620@gmail.com>
2017-09-30 22:10:20 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,23 @@
+dict1 = dict()
+print dict1
+
+dict2 ={'a':1,'b':2}
+print(dict2)
+
+dict2['a'] = 1000
+print dict2
+
+
+for i in dict2:
+    dict1[dict2[i]]= i
+
+print dict1
+
+print dict1.clear()
+print dict1
+
+print dict2.items()
+print dict2.keys()
+
+print("hello "*5)
+
